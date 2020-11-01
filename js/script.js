7e40c50d16ba50a2f91303fbc7c755bee986593d
Mike <mikhail.kbrv@gmail.com>
2020-11-01 22:55:18 +0500

    initial commit

--- js/script.js
+++ js/script.js
@@ -1,2 +1,22 @@
 "use strict"; 
 
+
+const numberOfFilms = +prompt('Number of films watched?');
+console.log(numberOfFilms);
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+var i;
+for (i = 0; i < 2; i++){
+    const lastFilm = prompt('Name one of the last watched films?');
+    const scoreFilm = prompt('How much you liked it from 1 to 10?');
+    personalMovieDB.movies[lastFilm] = scoreFilm;
+}
+
+console.log(numberOfFilms);
