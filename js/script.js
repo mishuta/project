f7924a61421ba81cedc22c78054ddb5d22e4ca30
Mike <mikhail.kbrv@gmail.com>
2020-11-02 18:03:13 +0500

    conditions and loops

--- js/script.js
+++ js/script.js
@@ -12,11 +12,27 @@ const personalMovieDB = {
     privat: false
 };
 
-var i;
-for (i = 0; i < 2; i++){
-    const lastFilm = prompt('Name one of the last watched films?');
-    const scoreFilm = prompt('How much you liked it from 1 to 10?');
-    personalMovieDB.movies[lastFilm] = scoreFilm;
+if (personalMovieDB.count < 10) {
+    console.log('Noob');
+} else if (personalMovieDB.count < 30) {
+    console.log('Mediocre');
+} else {
+    console.log('Omega');
+}
+
+
+for (let i = 0; i < 3; i++) {
+    const a = prompt('Name one of the last watched films?');
+    const b = prompt('How much you liked it from 1 to 10?');
+    if (a != null && b != null && a != '' && b != '' && a.length < 6 && b.length < 6 ) {
+        personalMovieDB.movies[a] = b;        
+        console.log('done');
+    } else {
+        i = 0;
+        console.log('error');        
+    }
+    
+    
 }
 
 console.log(personalMovieDB);
